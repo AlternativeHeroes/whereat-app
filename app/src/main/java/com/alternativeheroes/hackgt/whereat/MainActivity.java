68a1b5786f0955b77b86440c4acf0da9aed2bb7f
ynambiar <yamini.nambiar@gmail.com>
2014-09-20 13:28:23 -0400

    Hey2

--- app/src/main/java/com/alternativeheroes/hackgt/whereat/MainActivity.java
+++ app/src/main/java/com/alternativeheroes/hackgt/whereat/MainActivity.java
@@ -12,11 +12,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.CompoundButton;
 import android.widget.ListView;
-import android.widget.Toast;
 
 
-public class MainActivity extends FragmentActivity
-        implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends FragmentActivity implements ListView.OnItemClickListener {
 
     private LoginFragment loginFrag;
 
@@ -46,6 +44,7 @@ public class MainActivity extends FragmentActivity
         mDrawerList   = (ListView) findViewById(R.id.left_drawer);
 
         mDrawerList.setAdapter(new BlurbAdapter(this));
+        mDrawerList.setOnItemClickListener(this);
     }
 
 
@@ -61,37 +60,26 @@ public class MainActivity extends FragmentActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
-        ServerAPI.setCurrentOrdering(item.getTitleCondensed().toString());
         return super.onOptionsItemSelected(item);
     }
 
-    // On blurb change
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i("ASLCKH", "SLDJC");
+    }
+
     public void onBlurbChange(int blurbIndex, boolean checked, CompoundButton btnView) {
-        Toast.makeText(this, ServerAPI.getBlurbs()[blurbIndex], Toast.LENGTH_SHORT).show();
-        if (checked) {
-            ServerAPI.activateBlurb(blurbIndex);
-        }
-        else {
-            ServerAPI.deactivateBlurb(blurbIndex);
-        }
-        updateEventsListAdapter();
+        Log.i("Hello, ", "World!");
     }
 
-    // On Location Change
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(this, ServerAPI.getLocations()[position], Toast.LENGTH_SHORT).show();
-        ServerAPI.setCurrentLocation(position);
-        updateEventsListAdapter();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {  }
-
-    private void updateEventsListAdapter() {
+    public void onGroupItemClick(View v) {
 
     }
 }
